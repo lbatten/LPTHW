0fa57a1f461147974ca9dd75254e294ce3c99cf1
Ligaya <lbatten@me.com>
2018-01-10 00:06:06 +0300

    finished Ex4

--- Exercises/Ex4.py
+++ Exercises/Ex4.py
@@ -35,4 +35,26 @@ print "There are only", drivers, "drivers available."
 print "There will be", cars_not_driven, "empty cars today."
 print "We can transport", carpool_capacity, "people today."
 print "We have", passengers, "to carpool today."
-print "We need to put about", average_passengers_per_car, "in each car."
+print "We need to put about", average_passengers_per_car, "passengers in each car."
+
+# STUDY DRILLS #
+################
+# Set variable average_people_per_car to calculate the average number of people in a car including the driver
+# carpool capacity divided by number of cars driven
+average_people_per_car = carpool_capacity / cars_driven
+print "Which is", average_people_per_car, "in each car including the driver."
+
+# I carried you the test above to check that the problem wasn't with the logic, but with the syntax
+# The error in the teacher's line 8 (which corresponds to my line 31) is that
+# he set the variable as "carpool_cap" and then used "car_pool_cap" to call it
+# this meant that the system did not recognise the name of the variable called
+
+# What happens if we use 4 instead of 4.0 in line 7?
+# This does not seem to make any difference to the outcome, except for the "floating point"
+
+# Using python as a calculator #
+# $ cars = 100
+# $ drivers = 30
+# $ cars_not_driven = cars - drivers
+# $ print cars_not_driven
+# $ 70
